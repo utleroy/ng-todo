9b0df6c7d61684351a1119d65f9b674cf09644cb
Bryan Hankins <bryanhankins@me.com>
2016-11-14 21:33:53 -0600

    day1 down

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,40 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", function($scope) {
+	$scope.navItems=[{name:"Logout"},{name:"All Items"},{name:"New Item"}];
+});
 
+app.controller("TodoCtrl", function($scope) {
+	$scope.welcome = "ksdhfglkfds";
+	$scope.showListView = true;
+	$scope.items = [
+	{
+		id:0,
+		task:"mow the lawn",
+		isCompleted:true,
+		assignedTo:"Lee"
+	},
+	{
+		id:0,
+		task:"go home",
+		isCompleted:false,
+		assignedTo:"leroy"
+	},
+	{
+		id:0,
+		task:"code",
+		isCompleted:false,
+		assignedTo:"jimmy"
+	}
+	];
+
+	$scope.allItems=function(){
+	$scope.showListView = true;
+}
+$scope.newItem=function(){
+	$scope.showListView = false;	
+
+};
+});
